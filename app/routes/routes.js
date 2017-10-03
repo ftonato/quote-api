fa26ec3fe788fb8b6e4c80d68df126899b65bda0
Ademílson F. Tonato <ademilsonft@outlook.com>
2017-10-03 14:30:22 -0300

    update routes

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,36 +1,63 @@
 module.exports = function(app, db) {
-	const quoteRef = db.ref('quote');
-	const routes = {
-		quote: '/quote'
-	};
+  const quoteRef = db.ref('quotes');
+  const routes = {
+    quote: '/quote'
+  };
 
-	// TODO = add [get] here
-	app.get(routes.quote+'/:id', (req, res) => {
-	});
+  // TODO = add [get] here
+  app.get([routes.quote, routes.quote+'/:id'], (req, res) => {
+    var _quoteRef = quoteRef;
+    if (req.params.id !== undefined) {
+      _quoteRef = quoteRef.child(req.params.id);
+    }
 
-	// TODO = add [post] here
-	app.post(routes.quote, (req, res) => {
-	});
+    _quoteRef.once('value').then(function(snapshot) {
+      if (snapshot.val() == null) {
+        res.send({'error':'An error has occurred', '_error': 'No quote found with that ID'});
+      } else {
+        res.json(snapshot.val());
+      }
+    });
+  });
 
-	app.put(routes.quote+'/:id', (req, res) => {
+  // TODO = add [post] here
+  app.post(routes.quote, (req, res) => {
+    var data = {};
+    data.quote = req.body.text;
+    data.author = req.body.author;
 
-		var user = {};
-		user.quote = req.body.user.quote;
-		user.author = req.body.user.author;
+    quoteRef.push(data, function(err) {
+      if (err) {
+        res.send({'error':'An error has occurred', '_error': err});
+      } else {
+        res.json({ message: "Success: Quote created."});
+      }
+    });
+  });
 
-		quoteRef.push({
-			quote: user.quote,
-			author: user.author
-		}, function(err) {
-			if (err) {
-				res.send({'error':'An error has occurred', '_error': err});
-			} else {
-				res.json({ message: "Success: Quote created."});
-			}
-		});
-	});
+  // TODO = add [put] here
+  app.put(routes.quote+'/:id', (req, res) => {
+    var data = {};
+    data.quote = req.body.text;
+    data.author = req.body.author;
 
-	// TODO = add [delete] here
-	app.delete(routes.quote+'/:id', (req, res) => {
-	});
+    quoteRef.child(req.params.id).update(data, function(err) {
+      if (err) {
+        res.send({'error':'An error has occurred', '_error': err});
+      } else {
+        res.json({ message: "Success: Quote updated."});
+      }
+    });
+  });
+
+  // TODO = add [delete] here
+  app.delete(routes.quote+'/:id', (req, res) => {
+    quoteRef.child(req.params.id).remove(function(err) {
+      if (err) {
+        res.send({'error':'An error has occurred', '_error': err});
+      } else {
+        res.json({ message: "Success: Quote deleted."});
+      }
+    });
+  });
 };
